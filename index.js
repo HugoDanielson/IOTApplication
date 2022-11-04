1cc9d1fb032afec6ece25094103a55cea155a53e
DESKTOP-KPGRH37\hugod <danielson.hugo@outlook.com>
2022-11-04 15:19:04 +0100

    no message

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 
 
 
-app.use("/static",express.static("public"));
+app.use("/static", express.static("public"));
 app.set("view engine", "ejs");
 
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.render("todo.ejs");
-    });
+});
 
 
 
-app.listen(3000, () => console.log("Server Up and running"));
+app.listen(3000, () => {
+
+    console.log("Server Up and running");
+});
+
