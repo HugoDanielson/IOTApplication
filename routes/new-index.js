7a538cd7f9d80e31becfdaa0e2e91b4a74ce82c7
DESKTOP-KPGRH37\hugod <danielson.hugo@outlook.com>
2022-11-04 16:00:20 +0100

    no message

--- routes/new-index.js
+++ routes/new-index.js
@@ -3,13 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://iotapplication-database:ewCzPsbymBDeldkREoqoceQ3UWjKSKNrq2183ioCmrwfYEEPoJaeduBDUhEYRNmE4Qgos96qICgvCuwF6G5JWQ==@iotapplication-database.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@iotapplication-database@";
+var url = "mongodb+srv://<username>:<password>@<cluster>-vgz77.azure.mongodb.net/test?retryWrites=true&w=majority";
 
 
 router.get('/', (req, res, next) => {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("iotapplication-database");
+		var dbo = db.db("<database>");
 		dbo.collection("collection1").find({}).toArray(function(err, result) {
 			if (err) throw err;
 			console.log('Mongo data coming in hot')
